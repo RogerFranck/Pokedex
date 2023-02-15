58db1c73d4170ea041e2f9bb56163f82d1d7581b
miguel tostado <mtalamo2000@gmail.com>
2023-02-15 15:25:47 -0600

    import library KeyboardAwareScrollView

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import Pokedex from "./views/Pokedex";
+
 
 export default function App() {
   return (
+    <KeyboardAwareScrollView>
     <View style={styles.container}>
       <Pokedex />
       <StatusBar style="auto" />
     </View>
+    </KeyboardAwareScrollView>
   );
 }
 
